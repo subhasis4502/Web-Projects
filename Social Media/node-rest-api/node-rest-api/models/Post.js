e19a9b25674756bf0f3ca897458767cd600d95b4
subhasis4502 <subhasis4502@gmail.com>
2021-07-28 09:25:14 +0530

    first commit

--- Social Media/node-rest-api/node-rest-api/models/Post.js
+++ Social Media/node-rest-api/node-rest-api/models/Post.js
@@ -2,62 +2,23 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: {
+    userId:{
       type: String,
-      required: true,
-      min: 3,
-      max: 20,
-      unique: true,
+      required: true
     },
-    email: {
+    desc:{
       type: String,
-      required: true,
-      max: 50,
-      unique: true,
+      max: 500
     },
-    password: {
-      type: String,
-      required: true,
-      min: 6,
-    },
-    profilePicture: {
-      type: String,
-      default: "",
-    },
-    coverPicture: {
-      type: String,
-      default: "",
-    },
-    followers: {
-      type: Array,
-      default: [],
-    },
-    followings: {
-      type: Array,
-      default: [],
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
-    desc: {
-      type: String,
-      max:50
-    },
-    city:{
+    img:{
       type:String,
-      max:50
     },
-    from:{
-      type:String,
-      max:50
+    likes:{
+      type: Array,
+      default: []
     },
-    relationship:{
-      type:Number,
-      enum:[1,2,3]
-    }
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("Post", UserSchema);

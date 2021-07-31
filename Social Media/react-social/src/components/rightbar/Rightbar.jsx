542d802269b2bfe19c213c70cad8199699fb905f
subhasis4502 <subhasis4502@gmail.com>
2021-07-31 07:30:23 +0530

    first commit

--- Social Media/react-social/src/components/rightbar/Rightbar.jsx
+++ Social Media/react-social/src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,25 @@
 import "./rightbar.css";
+import { Users } from "../../dummyData";
+import Online from "../online/Online";
 
 export default function Ridebar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="/assets/gift.png" alt="" />
+                    <span className="birthdayText">
+                        <b>Ranajoy Mondal</b> and <b>3 others</b> have their Birthday today
+                    </span>
+                </div>
+                <img src="/assets/ad.png" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    {Users.map(u=>(
+                        <Online key={u.id} user={u}/>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
--- Social Media/react-social/src/components/sidebar/Sidebar.jsx
+++ Social Media/react-social/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import "./sidebar.css";
 import { RssFeed, Chat, VideoLibrary, Group, Bookmark, Help, Work, Event, School} from "@material-ui/icons";
+import { Users } from "../../dummyData";
+import CloseFriend from "../closeFriend/CloseFriend";
 
 export default function Sidebar() {
     return (
@@ -12,60 +14,43 @@ export default function Sidebar() {
                     </li>
                     <li className="sidebarListItem">
                         <Chat className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Chats</span>
+                        <span className="sidebarListItemText">Chat</span>
                     </li>
                     <li className="sidebarListItem">
                         <VideoLibrary className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Videos</span>
+                        <span className="sidebarListItemText">VideoLibrary</span>
                     </li>
                     <li className="sidebarListItem">
                         <Group className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Groups</span>
+                        <span className="sidebarListItemText">Group</span>
                     </li>
                     <li className="sidebarListItem">
                         <Bookmark className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Bookmarks</span>
+                        <span className="sidebarListItemText">Bookmark</span>
                     </li>
                     <li className="sidebarListItem">
                         <Help className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Questions</span>
+                        <span className="sidebarListItemText">Help</span>
                     </li>
                     <li className="sidebarListItem">
                         <Work className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Jobs</span>
+                        <span className="sidebarListItemText">Work</span>
                     </li>
                     <li className="sidebarListItem">
                         <Event className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Events</span>
+                        <span className="sidebarListItemText">Event</span>
                     </li>
                     <li className="sidebarListItem">
                         <School className="sidebarIcon"/>
-                        <span className="sidebarListItemText">Courses</span>
+                        <span className="sidebarListItemText">School</span>
                     </li>
                 </ul>
                 <button className="sidebarButton">Show More</button>
                 <hr className="sidebarHr"/>
                 <ul className="sidebarFriendList">
-                    <li className="sidebarFriend">
-                        <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Soumya</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Soumya</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Soumya</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Soumya</span>
-                    </li>
-                    <li className="sidebarFriend">
-                        <img src="./assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Soumya</span>
-                    </li>
+                    {Users.map(u=>(
+                        <CloseFriend key={u.id} user={u}/>
+                    ))}
                 </ul>
             </div>
         </div>

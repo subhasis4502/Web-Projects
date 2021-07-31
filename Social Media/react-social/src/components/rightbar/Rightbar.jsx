b36f4a67c9ba84716c77277ce477955c57c8ba68
subhasis4502 <subhasis4502@gmail.com>
2021-07-31 11:11:09 +0530

    first commit

--- Social Media/react-social/src/components/rightbar/Rightbar.jsx
+++ Social Media/react-social/src/components/rightbar/Rightbar.jsx
@@ -2,10 +2,11 @@ import "./rightbar.css";
 import { Users } from "../../dummyData";
 import Online from "../online/Online";
 
-export default function Ridebar() {
-    return (
-        <div className="rightbar">
-            <div className="rightbarWrapper">
+export default function Ridebar({profile}) {
+
+    const HomeRightbar = () => {
+        return (
+            <>
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="/assets/gift.png" alt="" />
                     <span className="birthdayText">
@@ -19,6 +20,62 @@ export default function Ridebar() {
                         <Online key={u.id} user={u}/>
                     ))}
                 </ul>
+            </>
+        );
+    };
+
+    const ProfileGightbar = () =>{
+        return (
+            <>
+                <h4 className="rightbarTitle">User information</h4>
+                <div className="rightbarInfo">
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City: </span>
+                        <span className="rightbarInfoValue">Howrah</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From: </span>
+                        <span className="rightbarInfoValue">India</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship: </span>
+                        <span className="rightbarInfoValue">Single</span>
+                    </div>
+                </div>
+                <h4 className="rightbarTitle">User friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Wasim Zaffer</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Wasim Zaffer</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Wasim Zaffer</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Wasim Zaffer</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Wasim Zaffer</span>
+                    </div>
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Wasim Zaffer</span>
+                    </div>
+                </div>
+            </>
+        )
+    }
+    return (
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <ProfileGightbar/>
             </div>
         </div>
     )
